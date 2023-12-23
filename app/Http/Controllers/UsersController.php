56ab4da1de131ad4260268500170cd0187c97dfb
sakakaito <k.bibifuu@gmail.com>
2023-12-23 17:33:34 +0900

    検索フォーム設置、ユーザー検索の処理（ユーザー全体表示自分も）

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
@@ -10,7 +11,14 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
-        return view('users.search');
+    public function search(Request $request){
+        $users = User::get();
+        $keyword = $request->input('keyword');
+    if(!empty($keyword)){
+        $users = User::where('username','like','%'.$keyword.'%')->get();
+    }else{
+        $users = User::all();
+    }
+        return view('users.search',['users'=>$users]);
     }
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -3,10 +3,20 @@
 @section('content')
 <div>
     <div>
-        <form action="" type="text" placeholder="ユーザー名"></form>
-        <button><img src="images/search.png" alt=""></button>
+        <form action="{{route('users.search')}}" method="post">
+            @csrf
+            <input type="text" name="keyword" placeholder="ユーザー名">
+            <button type="submit" class="search-btn"><img src="images/search.png" alt=""></button>
+            <p>検索ワード：</p>
+        </form>
     </div>
 </div>
-
-
+@foreach($users as $user)
+<div>
+    <tr>
+        <td>{{$user->images}}</td>
+        <td>{{$user->username}}</td>
+    </tr>
+</div>
+@endforeach
 @endsection
